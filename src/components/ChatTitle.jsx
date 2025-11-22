33c13fcc43a8dd7e0de68615bebe0d2613ce5700
FelixBruguera <felixbruguera0gmail.com>
2025-11-22 11:11:09 -0400

    fix text overflow in chat title

--- src/components/ChatTitle.jsx
+++ src/components/ChatTitle.jsx
@@ -8,15 +8,15 @@ const ChatTitle = ({ chat }) => {
     return null
   }
   return (
-    <div className="flex py-5 h-fit items-center justify-between gap-3 w-8/10 mx-auto">
+    <div className="flex py-5 h-fit items-start justify-between gap-3 w-8/10 mx-auto overflow-hidden">
       <h2
-        className={`max-w-7/10 text-base font-semibold text-justify ${!titleOpen && 'whitespace-nowrap text-ellipsis overflow-hidden'} mx-auto`}
+        className={`max-w-9/10 text-base font-semibold text-justify  mx-auto`}
       >
-        {titleOpen ? chat.title : chat.title.slice(0, 150)}
+        {titleOpen ? chat.title.slice(0, 5000) : chat.title.slice(0, 100)}
       </h2>
       {chat.title?.length > 100 ? (
         <Button
-          className="place-self-start"
+          className="place-self-center"
           variant="ghost"
           onClick={() => setTitleOpen((prev) => !prev)}
         >
